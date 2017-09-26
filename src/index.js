06f448bf8bf841647c23b5fe8f5447122cb44230
SchruteFarms23 <romanmullady@gmail.com>
2017-09-26 09:23:24 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+function clicked(){
+  console.log("hey")
+}
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={clicked} />
   </div>,
   document.getElementById('root')
 );
